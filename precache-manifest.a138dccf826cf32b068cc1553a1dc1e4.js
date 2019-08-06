97309c077991e93599d537476cbc2cda72783299
Michael Heffernan <michaelheffernan@Michaels-MacBook-Pro.local>
2019-08-05 20:10:19 -0400

    Updates

---
+++ precache-manifest.a138dccf826cf32b068cc1553a1dc1e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de362d15c4c2c5cf04f1ba1568b91dbb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c487e83253e8914ea098",
+    "url": "/monsters-rolodex/static/css/main.93a087e6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c487e83253e8914ea098",
+    "url": "/monsters-rolodex/static/js/main.823b3875.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
